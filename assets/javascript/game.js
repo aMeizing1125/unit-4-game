09f71bfaebf7f81b05748d6d770beaa5930b3eb4
aMeizing1125 <hampton.muriel@gmail.com>
2019-02-09 04:40:21 -0500

    First Commit. Mostly CSS. Little JavaScript

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+var hero = characters[i];
+var enemy = characters[i];
+
+var players = {
+    characterName1: {
+        attackPower: 50,
+        defend: 50,
+        forcePower: 50,
+        forceDefend: 50,
+        heroBonus: 5,
+        healthPoints: 150
+    },
+    characterName2: {
+        attackPower: 25,
+        defend: 75,
+        forcePower: 100,
+        forceDefend: 25,
+        heroBonus: 5,
+        healthPoints: 200
+    },
+    characterName3: {
+        attackPower: 30,
+        defend: 50,
+        forcePower: 30,
+        forceDefend: 100,
+        heroBonus: 5,
+        healthpoints: 250,
+    },
+    characterName4: {
+        attackpower: 80,
+        defend: 20,
+        forcePower: 80,
+        forceDefend: 35,
+        heroBonus: 5,
+        healthPoints: 125,
+    }
+}
+// select Hero - 
+$('.character').on('click', function(){
+    this.attr(data-player) function() {
+        
+    }
+})
+function selectHero
